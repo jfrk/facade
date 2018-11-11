f1aaf2d7f868bdc1470b13a101aa5283457c01c6
Fredrik Johansson <jeanfredrik@gmail.com>
2018-11-11 09:43:56 +0100

    🔥 Remove out-commented code

--- index.js
+++ index.js
@@ -1,97 +1,5 @@
 const decurry = require('decurry')
 
-// const arrayTraps = elementTraps => ({
-//   get(target, prop, wrapper) {
-//     // Handle indexes
-//     if (
-//       typeof prop === 'number' ||
-//       Number.isInteger(Number(prop))
-//     ) {
-//       return new Proxy(
-//         Reflect.get(target, prop, wrapper),
-//         elementTraps,
-//       )
-//     }
-//     // TODO: Add own traps (props and methods on the array)
-//     return Reflect.get(target, prop, wrapper)
-//   },
-//   // set(target, prop, wrapper) {
-//   //   if (
-//   //     typeof prop === 'number' ||
-//   //     Number.isInteger(Number(prop))
-//   //   ) {
-//   //     Reflect.set(target, prop, wrapper)
-//   //   }
-//   //   Reflect.set(target, prop, wrapper)
-//   // },
-// })
-
-// const wrapArray = ({ spec }) => array => {
-//   let elementTraps = {
-//     get(target, prop, wrapper) {
-//       let descriptor = Object.getOwnPropertyDescriptor(
-//         spec,
-//         prop,
-//       )
-//       if (!descriptor) {
-//         return Reflect.get(target, prop, wrapper)
-//       }
-//       return Reflect.get(spec, prop, wrapper)
-//     },
-//   }
-//   return new Proxy(array, arrayTraps(elementTraps))
-// }
-
-// const wrapObject = (spec, types = {}) => target => {
-//   let traps = {
-//     get(target, prop, wrapper) {
-//       let descriptor = Object.getOwnPropertyDescriptor(
-//         spec,
-//         prop,
-//       )
-//       // If no specification for this prop, just get it directly from the target.
-//       if (!descriptor) {
-//         return Reflect.get(target, prop, wrapper)
-//       }
-//       // If the spec has a getter for this prop, call it
-//       if (descriptor.get) {
-//         // Gets the prop from the spec with Reflect so that `this` will be the wrapper
-//         return Reflect.get(spec, prop, wrapper)
-//       }
-//       // If the spec has a function as the value for this prop, return it
-//       if (
-//         descriptor.value &&
-//         descriptor.value instanceof Function
-//       ) {
-//         return Reflect.get(spec, prop, wrapper)
-//       }
-//       if (
-//         descriptor.value &&
-//         typeof descriptor.value === 'string'
-//       ) {
-//         // TODO: Handle typerefs
-//         let type = types[descriptor.value]
-//         if (type) {
-//           return wrapObject(type, types)(
-//             Reflect.get(target, prop, wrapper),
-//           )
-//         }
-//       }
-//     },
-//     set(target, prop, value, wrapper) {
-//       let descriptor = Object.getOwnPropertyDescriptor(
-//         spec,
-//         prop,
-//       )
-//       if (descriptor && descriptor.set) {
-//         return Reflect.set(spec, prop, value, wrapper)
-//       }
-//       return Reflect.set(target, prop, value, wrapper)
-//     },
-//   }
-//   return new Proxy(target, traps)
-// }
-
 const INDEX = Symbol('numericalIndex')
 const ROOT = Symbol('rootObject')
 const PARENT = Symbol('parentObject')
@@ -205,8 +113,6 @@ module.exports = wrap
 
 Object.assign(module.exports, {
   wrap,
-  // wrapArray,
-  // wrapObject,
   INDEX,
   ROOT,
   PARENT,
